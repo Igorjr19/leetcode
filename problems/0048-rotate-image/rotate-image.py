bac0105a2011226ad2d0638d3859d1f1e0a0a3c7
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-02-04 09:28:22 -0300

    fix solution for problem 48

--- problems/0048-rotate-image/rotate-image.py
+++ problems/0048-rotate-image/rotate-image.py
@@ -1,8 +1,6 @@
 import numpy as np
 
 class Solution:
-    rotation_matrix = np.array([[0, -1], [-1, 0]])
-
     def rotate(self, matrix: List[List[int]]) -> None:
         reflection_matrix = np.eye(len(matrix))[::-1].astype(int)
         
