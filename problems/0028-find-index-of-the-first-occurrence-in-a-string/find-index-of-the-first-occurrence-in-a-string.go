989b00c0cada8cf1d38a796af0295903bdd97cdb
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-02-04 13:33:22 -0300

    fix solution for problem 28

---
+++ problems/0028-find-index-of-the-first-occurrence-in-a-string/find-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,17 @@
+func strStr(haystack string, needle string) int {
+	for i := 0; i < len(haystack)-(len(needle)-1); i++ {
+		found := true
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				found = false
+				break
+			}
+		}
+
+		if found {
+			return i
+		}
+	}
+
+	return -1
+}
