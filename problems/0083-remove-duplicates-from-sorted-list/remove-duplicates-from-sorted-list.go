59c01a71c51f6aea734a2f5d6ace7e5952a8dbe7
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-02-09 16:49:02 -0300

    add solution for problem 83

---
+++ problems/0083-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	if head.Val == head.Next.Val {
+		head.Next = head.Next.Next
+		return deleteDuplicates(head)
+	}
+
+	head.Next = deleteDuplicates(head.Next)
+
+	return head
+}
